117afa22ae6a73186e2bd20ddff77774c6a1211d
Alex Alonso <alex4551@gmail.com>
2017-08-03 17:17:55 +0800

    add test

---
+++ src/__test__/normalizeport.test.ts
@@ -0,0 +1,19 @@
+import {normalizePort} from "./../../dist/index.js";
+
+describe("test valid port", () => {
+    test("3000 return 3000", () => {
+        expect(normalizePort(3000)).toBe(3000);
+    });
+    test("0 return false", () => {
+        expect(normalizePort(0)).toBe(false);
+    });
+    test("65535 return false", () => {
+        expect(normalizePort(65535)).toBe(false);
+    });
+    test("asd String return false", () => {
+        expect(normalizePort("asd")).toBe(false);
+    });
+    test("'3000' return 3000", () => {
+        expect(normalizePort("3000")).toBe(3000);
+    });
+});
