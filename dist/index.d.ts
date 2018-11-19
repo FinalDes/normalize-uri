d661afa4e3e59e0631e03e10bad1dcfd0027633c
Alex Alonso <alex4551@gmail.com>
2018-11-19 14:04:44 +0800

    update node packages

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function normalizeMongoURI({host, port, db, user, pass}: {
-    host: string;
-    port?: number | string;
-    db?: string;
-    user?: string;
-    pass?: string;
-}): string;
-export declare function normalizePort(port: number | string): number | boolean;
